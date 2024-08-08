f685156d7cfd0c5c1af9dabf30c39c272c8eecc0
SnehalDiwan <snehalkulkarni900@gmail.com>
2024-08-08 16:27:50 +0530

    added fb page

---
+++ src/XpathPractice.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class XpathPractice {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\shubh\\Documents\\SnehalCoding\\Selenium\\SeleniumFirst\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.facebook.com/");
+        driver.findElement(By.xpath(("//input[@id='email']"))).sendKeys("Snehal");
+        driver.findElement(By.xpath(("//input[@id='pass']"))).sendKeys("pass@123");
+        driver.findElement(By.xpath(("//button[@type='submit']"))).click();;
+      
+    }
+}
