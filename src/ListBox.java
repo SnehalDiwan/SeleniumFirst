8e74cd57249a25220374915549282f6aa096f8d6
SnehalDiwan <snehalkulkarni900@gmail.com>
2024-08-09 14:00:09 +0530

    added list box

---
+++ src/ListBox.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class ListBox {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\shubh\\Documents\\SnehalCoding\\Selenium\\SeleniumFirst\\chromedriver.exe");
+        WebDriver driver= new ChromeDriver();
+        driver.get("https://www.facebook.com/");
+        Thread.sleep(4000);
+        driver.findElement(By.xpath("//a[text()='Create new account']")).click();
+        Thread.sleep(2000);
+        WebElement month = driver.findElement(By.xpath("//select[@id='month']"));
+         Select s= new Select(month);
+         s.selectByIndex(1);
+         driver.close();
+    }
+}
