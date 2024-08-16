32c4ce09ea379dc5f344414a42b3a1b36d10250a
SnehalDiwan <snehalkulkarni900@gmail.com>
2024-08-16 16:29:15 +0530

    added new changes

---
+++ src/SauceDemo.java
@@ -0,0 +1,42 @@
+import java.io.InterruptedIOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+import org.openqa.selenium.support.ui.Select;
+
+
+public class SauceDemo {
+    public static void main(String[] args) throws InterruptedException{
+        // System.setProperty("webdriver.chrome.driver", "C:\\Users\\shubh\\Documents\\SnehalCoding\\Selenium\\SeleniumFirst\\chromedriver.exeull");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\shubh\\Documents\\SnehalCoding\\Selenium\\SeleniumFirst\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        Thread.sleep(2000);
+        driver.manage().window().maximize();
+        // driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//input[@name='user-name']")).sendKeys("standard_user");
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("//input[@id='login-button']")).click();
+        Thread.sleep(3000);
+       WebElement dropdown= driver.findElement(By.xpath("//select[@class='product_sort_container']"));
+        Select s= new Select(dropdown);
+        
+        s.selectByIndex(2);
+        Thread.sleep(2000);
+        List<WebElement> items = driver.findElements(By.xpath("//div[@class='inventory_item']"));
+        for(WebElement item : items){
+            System.out.println(items);
+        }
+      
+
+        driver.close();
+    }
+
+}
