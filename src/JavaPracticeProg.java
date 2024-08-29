2b57ead007e5140910adcb615e96d9e775fce382
SnehalDiwan <snehalkulkarni900@gmail.com>
2024-08-29 14:18:47 +0530

    new added program

---
+++ src/JavaPracticeProg.java
@@ -0,0 +1,17 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class JavaPracticeProg {
+    public static void main(String[] args) {
+        String str1="now";
+        String str2="own";
+
+        char[] char1=str1.toCharArray();
+        char[] char2=str2.toCharArray();
+        Arrays.sort(char1);
+        Arrays.sort(char2);
+        System.out.println(Arrays.equals(char1, char2));
+        
+    }
+    
+}
